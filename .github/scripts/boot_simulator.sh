d440572787d450949015ecca6a9b5393d2fdf5a4
Amanjeet Singh <amanjeetsingh150@gmail.com>
2024-12-15 20:09:06 +0530

    fix: update macOS and xcode version to fix build

--- .github/scripts/boot_simulator.sh
+++ .github/scripts/boot_simulator.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Specify the device type and runtime as per your requirements
-DEVICE_TYPE="iPhone 14"
-RUNTIME="iOS17.2"
+DEVICE_TYPE="iPhone 15"
+RUNTIME="iOS17.5"
 
 # Create a unique identifier for the new simulator to avoid naming conflicts
 SIMULATOR_NAME="Simulator_$(uuidgen)"
